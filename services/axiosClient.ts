21da4513042f1341288383b300834c9754efe5ff
Suprim Khadka <sups11996@gmail.com>
2026-09-07 15:07:15 +0545

    fix: removed public from apis

--- services/axiosClient.ts
+++ services/axiosClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
+const BASE_URL = process.env.NEXT_FRONTEND_URL;
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
