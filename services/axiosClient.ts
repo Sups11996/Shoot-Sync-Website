6e9c82a3fe454c3ffda9be32990d108468841114
Suprim Khadka <sups11996@gmail.com>
2026-09-07 15:30:51 +0545

    fix: removed frontend from the env and replaced with public  backend

--- services/axiosClient.ts
+++ services/axiosClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.NEXT_BACKEND_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
